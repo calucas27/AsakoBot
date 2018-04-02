bb7a47f27d91e96ff033de7ae0102639ca5104b0
calucas27 <chaselucas265@gmail.com>
2018-04-01 19:42:52 -0500

    Update asako.rb

--- asako.rb
+++ asako.rb
@@ -1,9 +1,8 @@
 #Asako:  A simple Discord Bot written in Ruby.
 #Author: https://github.com/calucas27
-
 require 'discordrb'
 
-bot = Discordrb::Bot.new token:'YOURTOKENGOESHERE'
+bot = Discordrb::Bot.new token:'<|Your Token Goes Here|>'
 
 bot.message(content: 'Asako') do |event|
     event.respond 'Hello, my name is Asako!  Check out https://github.com/calucas27 for more cool stuff!'
@@ -13,8 +12,11 @@ bot.message(content: 'Asako help') do |event|
     commands = %{
         *Here's my commands! -- Mention me and I can reply back to you!*
         `Asako`: Say hello to Asako!
-        `help`: Displays this list again.
-        `voice`: Join the currently active voice channel and play a song.
+        `Asako help`: Displays this list again.
+        `Asako gitsearch`: Search github for a given username.
+        `Asako join channel`: Joins the bot to the current voice channel.
+        `Asako play song`: Play's everyone's favorite CCDC cleanup song.
+        `Asako stop song`: Kills the current voice session.
     }
     event.respond(commands)
 end
@@ -25,12 +27,42 @@ end
 
 bot.message(start_with: "Asako gitsearch") do |event|
     query = event.message.content
-    query_split = query.split(' ')
-    query_split = query_split.last
-    puts query_split
+    query_split = query.split(' ').last
     url = 'https://github.com/'
     url.concat(query_split)
     event.respond(url)
+end
+
+bot.message(content: "Asako join channel") do |event|
+    channel = event.user.voice_channel
+    next "You're not in a voice channel!" unless channel
+
+    bot.voice_connect(channel)
+    "Connected to voice channel: #{channel.name}"
+end
+
+bot.message(content: "Asako play song") do |event|
+    #if not already in voice channel
+    channel = event.user.voice_channel
+    next "You're not in a voice channel!" unless channel
+
+    voice_bot = event.voice
+    event.respond("Here's your music!~")
+    voice_bot.play_file('epicsaxguy.mp3')
+
+    if voice_bot.isplaying? == false
+        event.respond("Your song has ended!")
+        bot.voice_destroy(channel)
+    end
+
+end
+
+bot.message(content: "Asako leave channel") do |event|
+    channel = event.user.voice_channel
+    next "You're not in a voice channel!" unless channel
+
+    event.respond("That's enough music for now...")
+    bot.voice_destroy(channel)
 end
 
 bot.mention do |event|
